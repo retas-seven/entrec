311f3ae98c61263c4f6bb4f3e9cd90719a7293bc
t-osawa <retas_seven@hotmail.co.jp>
2017-08-29 21:25:48 +0900

    更新年月日のデフォルト値を'0000-00-00 00:00:00'からCURRENT_TIMESTAMPに変更

--- ddl/create.sql
+++ ddl/create.sql
@@ -16,7 +16,7 @@ CREATE TABLE entrec.user (
   , password		varchar(100)	NOT NULL								COMMENT 'パスワード'
   , regist_date		timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP		COMMENT '登録年月日'
   , regist_user_id	varchar(12)		NOT NULL								COMMENT '登録ユーザID'
-  , update_date		timestamp		NOT NULL DEFAULT '0000-00-00 00:00:00' 	COMMENT '更新年月日'
+  , update_date		timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP		COMMENT '更新年月日'
   , update_user_id	varchar(12)		NOT NULL								COMMENT '更新ユーザID'
   , version			int(11)			NOT NULL DEFAULT '0'					COMMENT 'バージョン（楽観ロック用）'
   , PRIMARY KEY (user_id)
@@ -32,7 +32,7 @@ CREATE TABLE entrec.user_cookie (
   , cookie_value 		varchar(100) 	NOT NULL								COMMENT 'クッキー値'
   , regist_date 		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP		COMMENT '登録年月日'
   , regist_user_id 		varchar(12) 	NOT NULL 								COMMENT '登録ユーザID'
-  , update_date 		timestamp 		NOT NULL DEFAULT '0000-00-00 00:00:00' 	COMMENT '更新年月日'
+  , update_date 		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP		COMMENT '更新年月日'
   , update_user_id 		varchar(12) 	NOT NULL 								COMMENT '更新ユーザID'
   , version 			int(11) 		NOT NULL DEFAULT '0' 					COMMENT 'バージョン（楽観ロック用）'
   , PRIMARY KEY (user_id,cookie_name,cookie_value)
@@ -58,7 +58,7 @@ CREATE TABLE entrec.check_history (
   , door_window_lock 		bit(1) 			NOT NULL DEFAULT b'0'					COMMENT '戸・窓の施錠'
   , regist_date 			timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP		COMMENT '登録年月日'
   , regist_user_id 			varchar(12) 	NOT NULL 								COMMENT '登録ユーザID'
-  , update_date				timestamp 		NOT NULL DEFAULT '0000-00-00 00:00:00'	COMMENT '更新年月日'
+  , update_date				timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP		COMMENT '更新年月日'
   , update_user_id 			varchar(12) 	NOT NULL 								COMMENT '更新ユーザID'
   , version 				int(11) 		NOT NULL DEFAULT '0' 					COMMENT 'バージョン（楽観ロック用）'
   , PRIMARY KEY (check_year,check_month,check_day,chack_date_seq)
