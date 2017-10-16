21b660c06aea8bcc4006e1091ec517854c04e65d
t-osawa <retas_seven@hotmail.co.jp>
2017-10-16 23:49:12 +0900

    一覧表ヘッダのfixedを設定

--- src/main/webapp/resources/js/check_list.js
+++ src/main/webapp/resources/js/check_list.js
@@ -18,7 +18,23 @@ $(document).ready(function(){
         }
     }
     jQuery(window).scroll(floatMenu);
-    jQuery('body').bind('touchmove', floatMenu);
+
+	//------------------------------------------------
+	// 一覧表ヘッダのfixed
+	//------------------------------------------------
+    var tbl = jQuery('#tbl');
+    var header = jQuery('#tbl_header_only');
+    var tblTop = tbl.offset().top;
+    var floatHeader = function() {
+        // スクロール位置による固定
+        if (jQuery(window).scrollTop() > tblTop) {
+        	header.addClass('fixed_header');
+        } else {
+        	header.removeClass('fixed_header');
+        }
+    }
+    jQuery(window).scroll(floatHeader);
+
 });
 
 function saveSession() {
