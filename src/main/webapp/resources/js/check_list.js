402943c0d7fd2f5066fad867079ef9b229d9d512
t-osawa <retas_seven@hotmail.co.jp>
2017-10-16 23:18:16 +0900

    一覧表のStickyを廃止、カレンダーをStickyにした

--- src/main/webapp/resources/js/check_list.js
+++ src/main/webapp/resources/js/check_list.js
@@ -3,6 +3,25 @@ $(document).ready(function(){
 	// セッションタイムアウト防止用のタイマー
 	//------------------------------------------------
 	setInterval("saveSession()", 55 * 60 * 1000);
+
+	//------------------------------------------------
+	// 一覧のタイトル行のfixed
+	//------------------------------------------------
+    var nav = jQuery('#right_component');
+
+    // メニューのtop座標を取得する
+    var offsetTop = nav.offset().top - 45;
+
+    var floatMenu = function() {
+        // スクロール位置がメニューのtop座標を超えたら固定にする
+        if (jQuery(window).scrollTop() > offsetTop) {
+        	nav.addClass('fixed');
+        } else {
+        	nav.removeClass('fixed');
+        }
+    }
+    jQuery(window).scroll(floatMenu);
+    jQuery('body').bind('touchmove', floatMenu);
 });
 
 function saveSession() {
